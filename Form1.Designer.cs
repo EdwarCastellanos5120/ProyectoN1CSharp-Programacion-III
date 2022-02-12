a951f78eb24519c6988018ef04a6e8901e38d178
Stiven Ramos <sramosh1@miumg.edu.gt>
2022-02-11 21:26:01 -0600

    Tercer commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,52 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.combo1 = new System.Windows.Forms.ComboBox();
+            this.combo2 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // combo1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(43, 28);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(144, 28);
-            this.comboBox1.TabIndex = 2;
+            this.combo1.FormattingEnabled = true;
+            this.combo1.Location = new System.Drawing.Point(38, 21);
+            this.combo1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.combo1.Name = "combo1";
+            this.combo1.Size = new System.Drawing.Size(126, 23);
+            this.combo1.TabIndex = 2;
             // 
-            // comboBox2
+            // combo2
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(281, 28);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(144, 28);
-            this.comboBox2.TabIndex = 3;
+            this.combo2.FormattingEnabled = true;
+            this.combo2.Location = new System.Drawing.Point(246, 21);
+            this.combo2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.combo2.Name = "combo2";
+            this.combo2.Size = new System.Drawing.Size(126, 23);
+            this.combo2.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(431, 29);
+            this.textBox1.Location = new System.Drawing.Point(377, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(62, 27);
+            this.textBox1.Size = new System.Drawing.Size(55, 23);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(193, 28);
+            this.textBox2.Location = new System.Drawing.Point(169, 21);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(59, 27);
+            this.textBox2.Size = new System.Drawing.Size(52, 23);
             this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Presioname";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.combo2);
+            this.Controls.Add(this.combo1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -81,9 +99,10 @@
         }
 
         #endregion
-        private ComboBox comboBox1;
-        private ComboBox comboBox2;
+        private ComboBox combo1;
+        private ComboBox combo2;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,24 @@ namespace ProyectoN1
             String path = "C:\\Tareas\\ProyectoN1\\Resultados.json";
             String descarga = File.ReadAllText(path);
             equipos = JsonConvert.DeserializeObject<String[,]>(descarga);
+
+            
+            
+
+
+
+
             InitializeComponent();
 
+
         }
-       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < equipos.GetLength(0); i++)
+            { combo1.Items.Add(equipos[i, 1]); combo2.Items.Add(equipos[i, 1] ); }
+
+            textBox2.Text=Convert.ToString(combo1.SelectedItem);
+        }
     }
 }
