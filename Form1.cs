4774c3f5462bff8108ab5119a9ef3fdc7d95b971
Edwar <79104426+EdwarCastellanos5120@users.noreply.github.com>
2022-02-11 20:49:34 -0600

    Segundo Commit

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,23 @@
+using Newtonsoft.Json;
+
 namespace ProyectoN1
 {
     public partial class Form1 : Form
     {
+        //Variable Global
+        String[,] equipos;
+
+
+
         public Form1()
         {
+            //Lineas De Codigo Encargadas de Subir la Info
+            String path = "C:\\Tareas\\ProyectoN1\\Resultados.json";
+            String descarga = File.ReadAllText(path);
+            equipos = JsonConvert.DeserializeObject<String[,]>(descarga);
             InitializeComponent();
+
         }
+       
     }
 }
