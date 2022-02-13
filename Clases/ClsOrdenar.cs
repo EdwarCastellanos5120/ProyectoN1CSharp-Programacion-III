55113f266e8bb973d791412f9c3f56f4cf497d94
Stiven Ramos <sramosh1@miumg.edu.gt>
2022-02-12 23:05:06 -0600

    Solo tenemos la clase de Burbuja

---
+++ Clases/ClsOrdenar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoN1.Clases
+{
+    internal class ClsOrdenar
+    {
+
+
+        public void MetodoBurbuja()
+        {
+            int t;
+            for (int a = 1; a < vector.Length; a++)
+                for (int b = vector.Length - 1; b >= a; b--)
+                {
+                    if (vector[b - 1] < vector[b])
+                    {
+                        t = vector[b - 1];
+                        vector[b - 1] = vector[b];
+                        vector[b] = t;
+                    }
+                }
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+       
+
